acb7065ed21de26332cd33fa0f6e9864b65e1895
white160114 <2220128@ecc.ac.jp>
2024-03-08 16:24:38 +0900

    sqlファイル追加

---
+++ sql/chat.sql
@@ -0,0 +1,13 @@
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT,
+    content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
