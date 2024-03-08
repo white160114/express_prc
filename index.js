e62cfe268e24aaaca3527a593a58e89a42d54197
white160114 <2220128@ecc.ac.jp>
2024-03-08 14:39:58 +0900

    ファイル追加

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(3000, () => {
+    console.log(`Server started! click here 'http://localhost:3000'`);
+  });
