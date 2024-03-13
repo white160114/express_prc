70cba4f882759fad6078bb6d1fb2acb7be52737f
white160114 <2220128@ecc.ac.jp>
2024-03-13 16:22:46 +0900

    sql文の追加

--- sql/chat.sql
+++ sql/chat.sql
@@ -7,7 +7,17 @@ CREATE TABLE posts (
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT,
+    user_id INT,
     content TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE users(
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name varchar(20) not null,
+    password varchar(256) not null,
+    mail varchar(256) not null,
+
 );
